29911dfe6b6f9e69727293eee3e56177cf343363
Hoàng Phúc <113100226+hoangphuc38@users.noreply.github.com>
2024-10-30 13:55:59 +0700

    Add Dependency Injection for DataAccess and Application

---
+++ ShopManagement_Backend/ShopManagement_Backend_Application/ApplicationDependencyInjection.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ShopManagement_Backend_Application.MappingProfiles;
+using ShopManagement_Backend_Application.Services;
+using ShopManagement_Backend_Application.Services.Interfaces;
+
+namespace ShopManagement_Backend_Application
+{
+    public static class ApplicationDependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddServices();
+
+            services.AddAutoMapper();
+
+            return services;
+        }
+
+        private static void AddServices(this IServiceCollection services)
+        {
+            services.AddScoped<IProductService, ProductService>();
+            services.AddScoped<IShopService, ShopService>();
+            services.AddScoped<IShopDetailService, ShopDetailService>();
+            services.AddScoped<IUserService, UserService>();
+        }
+
+        private static void AddAutoMapper(this IServiceCollection services)
+        {
+            services.AddAutoMapper(typeof(IMappingProfileMarker));
+        }
+    }
+}
---
+++ ShopManagement_Backend/ShopManagement_Backend_DataAccess/DataAccessDependencyInjection.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ShopManagement_Backend_DataAccess.Persistance;
+using ShopManagement_Backend_DataAccess.Repositories;
+using ShopManagement_Backend_DataAccess.Repositories.Interfaces;
+
+namespace ShopManagement_Backend_DataAccess
+{
+    public static class DataAccessDependencyInjection
+    {
+        public static IServiceCollection AddDataAccess(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDatabase(configuration);
+
+            services.AddRepositories();
+
+            return services;
+        }
+
+        private static void AddRepositories(this IServiceCollection services)
+        {
+            services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped<IShopRepository, ShopRepository>();
+            services.AddScoped<IShopDetailRepository, ShopDetailRepository>();
+            services.AddScoped<IUserRepository, UserRepository>();
+        }
+
+        private static void AddDatabase(this IServiceCollection services, IConfiguration configuration)
+        {
+            var databaseConfig = configuration.GetConnectionString("ShopManagement"); ;
+
+            services.AddDbContext<ShopManagementDbContext>(options =>
+            {
+                options.UseSqlServer(databaseConfig);
+            });
+        }
+    }
+}
